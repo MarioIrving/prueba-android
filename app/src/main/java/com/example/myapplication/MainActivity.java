f740dc33e6576f47d08d425dd6581f9343de0c75
mario irving <marioirving.morales@gmail.com>
2021-09-24 14:30:41 +0200

    initial commit hito 24sep

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView texto;
+    private Button boton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        texto = findViewById(R.id.texto_hito);
+        boton = findViewById(R.id.boton_hito);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                texto.setText("Hola amigos");
+            }
+        });
+    }
+}
